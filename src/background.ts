c64731354d1bc8089de1253bf8137c4cdea362d9
Pranav Karthikeyan <pkarthikeyan@ttsiglobal.com>
2023-03-01 18:25:38 -0500

    Added basic right click functionality that appends download links to a list that will eventually be displayed

--- src/background.ts
+++ src/background.ts
@@ -1,3 +1,22 @@
 import React from "react";
 
 console.log("welcome to uplink from console");
+let links: string[] = ["String"];
+
+chrome.contextMenus.removeAll(function() {
+    chrome.contextMenus.create({
+        id: "rightclickoption",
+        title: "Save PDF",
+        contexts:["link"],
+       });
+});
+
+chrome.contextMenus.onClicked.addListener(function(info,tab){
+    let link:string = info.linkUrl as string;
+    links.push(link);
+    if(links[0] == "String")
+    {
+        links.shift();
+    }
+    console.log(links)
+});
--- src/right_click.ts
+++
@@ -1,9 +0,0 @@
-chrome.contextMenus.create({
-    title: "Save PDF",
-    contexts:["selection"],  
-    onclick: savePDF
-   });
-
-function savePDF(): void{
-    console.log("Right Click Option Selected")
-}
