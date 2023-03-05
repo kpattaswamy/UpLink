0fbb0e9f42a91bb5a9f15429b896e94fca401156
kpattaswamy <adhokshaz@gmail.com>
2023-03-05 18:38:09 -0500

    Changed view state to be stored in chrome.storage.session
    Signed-off-by: kpattaswamy <adhokshaz@gmail.com>

--- src/storage/store_view_state.ts
+++ src/storage/store_view_state.ts
@@ -5,14 +5,14 @@ export class ViewStateStorage {
     static putViewState(value:string){
         // This function adds the key:value => VIEW_STATE_KEY:'ui state' to storage
 
-        chrome.storage.local.set({ [VIEW_STATE_KEY]: value });
+        chrome.storage.session.set({ [VIEW_STATE_KEY]: value });
     }
     
     static getViewState(onGetStorageKeyValue:(s:string)=>void){
         // This function gets the stored view state from chrome's storage
         // The param is a function which sets the state, it acts like call back
 
-        chrome.storage.local.get([VIEW_STATE_KEY], function(result) {
+        chrome.storage.session.get([VIEW_STATE_KEY], function(result) {
            onGetStorageKeyValue(result[VIEW_STATE_KEY]);
         });
     }
@@ -20,7 +20,7 @@ export class ViewStateStorage {
     static removeViewState(onGetStorageKeyValue:()=>void){
         // This function removes view state from chrome's storage
 
-        chrome.storage.local.remove([VIEW_STATE_KEY], function() {
+        chrome.storage.session.remove([VIEW_STATE_KEY], function() {
             onGetStorageKeyValue();
         });
     }
