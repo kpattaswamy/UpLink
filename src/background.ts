d8a931919c378503e9083c44d4bd78374f46f93e
Pranav Karthikeyan <pkarthikeyan@ttsiglobal.com>
2023-04-02 17:01:02 -0400

    used session storage to allow for right click only during file transfer

--- src/background.ts
+++ src/background.ts
@@ -1,13 +1,21 @@
 import React from "react";
 import { URLStorage } from "./storage/store_url_array";
 
+const VIEW_STATE_KEY = 'viewState';
+
 //Creates a Context Menu right click option for saving pdf links
-chrome.contextMenus.removeAll(function() {
-    chrome.contextMenus.create({
-        id: "rightclickoption",
-        title: "Send with Uplink",
-        contexts:["link"],
-       });
+chrome.storage.session.get([VIEW_STATE_KEY], function(result) {
+    let viewState = result[VIEW_STATE_KEY];
+    if(viewState === 'file-transfer')
+    {
+        chrome.contextMenus.removeAll(function() {
+            chrome.contextMenus.create({
+                id: "rightclickoption",
+                title: "Send with Uplink",
+                contexts:["link"],
+               });
+        });
+    }
 });
 
 //Listener for right click option that stores the link in memory
