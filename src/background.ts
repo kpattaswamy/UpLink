fd861f8246c5af8f3154b768b595ba648ac734fa
Pranav Karthikeyan <pkarthikeyan@ttsiglobal.com>
2023-04-03 14:11:04 -0400

    removed unnecessary code

--- src/background.ts
+++ src/background.ts
@@ -1,10 +1,6 @@
 import React from "react";
 import { URLStorage } from "./storage/store_url_array";
 
-const VIEW_STATE_KEY = 'viewState';
-
-
-
 //Creates a Context Menu right click option for saving pdf links
 chrome.contextMenus.removeAll(function() {
     chrome.contextMenus.create({
